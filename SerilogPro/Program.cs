a9bece92a345945c4cd54c0b9121a6c07936c96d
cyxhub <1839914189@qq.com>
2020-05-27 11:10:51 +0800

    serilog

---
+++ SerilogPro/Program.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace SerilogPro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //直观的方式
+            /*Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("D:\\log\\myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Hello, world!");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
+            Console.ReadKey();*/
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+                    .ReadFrom.Configuration(hostingContext.Configuration)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console());
+                });
+    }
+}
---
+++ SerilogPro/readme.txt
@@ -0,0 +1,10 @@
+﻿https://www.xcode.me/post/5825
+https://www.jianshu.com/p/fd7898e64579
+加入包
+Install-Package Serilog
+Install-Package Serilog.Sinks.Console
+
+Install-Package Serilog.Sinks.File
+package Serilog.Sinks.RollingFile
+在Program中配置
+并在appsettings配置
